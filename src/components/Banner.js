a8ea580f3b0238ad4d4ea044c440067dcf01c40a
ibnubachdar <ibnubachdar@gmail.com>
2026-02-26 18:38:03 +0700

    feat(banner): implement mobile responsive banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,16 +3,16 @@ import React from 'react'
 export default function Banner() {
   return (
     // <!-- Banner -->
-    <div className="bg-[#E4BBA1] flex items-center justify-between px-14">
+    <div className="bg-[#E4BBA1] flex md:flex-row flex-col-reverse items-center justify-between px-14 py-3">
       {/* <!-- Judul & Tombol Banner  --> */}
-      <div>
-        <p className="text-3xl font-light text-black">Diskon 30% Hari ini</p>
-        <p className="text-6xl font-black text-[#003E29]">
+      <div className='text-center'>
+        <p className="text-3xl font-light text-black mb-3">Diskon 30% Hari ini</p>
+        <p className="md:text-6xl font-black text-[#003E29] text-3xl">
           Promo 2025 <br />
           Jacket Collection
         </p>
         <button
-          className="text-xl font-bold text-black bg-white py-4 px-6 mt-8 rounded-xl"
+          className="text-xl font-bold text-black bg-white py-4 px-6 mt-3 rounded-xl"
         >
           Beli Sekarang
         </button>
@@ -20,8 +20,8 @@ export default function Banner() {
       {/* <!-- Tutup Judul & Tombol Banner --> */}
 
       {/* <!-- Gambar Banner  --> */}
-      <div>
-        <img src="gambar/hero.png" className="h-[544px]" />
+      <div className=''>
+        <img src="gambar/hero.png" className="md:h-[544px] h-[244px] mb-10" />
       </div>
       {/* <!-- Tutup Gambar Banner  --> */}
     </div>
